5a1cc8ef3965958ab8fb5a8f791254f781e3476a
enginocal <e.isaocal@gmail.com>
2017-11-14 15:43:10 +0300

    Initial benchmark

---
+++ benchmarkdotnetsample/PerformanceProblem.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace benchmarkdotnetsample
+{
+    public class PerformanceProblem
+    {
+        public PerformanceProblem()
+        {
+
+        }
+        [Benchmark]
+        public string StringConcat()
+        {
+            return String.Concat(Enumerable.Repeat("a", 1000).ToArray());
+        }
+
+        [Benchmark]
+        public string StringBuilder()
+        {
+            StringBuilder sb = new System.Text.StringBuilder();
+            sb.Append('a', 1000);
+            return sb.ToString();
+        }
+
+    }
+}
---
+++ benchmarkdotnetsample/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace benchmarkdotnetsample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run(typeof(PerformanceProblem));
+        }
+    }
+}
